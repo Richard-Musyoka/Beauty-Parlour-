964240de8e5f283109a611d97236409700780d13
Dev_Richard <haimestoneritchie@gmil.com>
2025-01-13 13:55:54 +0300

    Changes on  APi

--- services.php
+++ services.php
@@ -97,7 +97,7 @@ $cnt=$cnt+1;
   <script src="js/bootstrap-datepicker.js"></script>
   <script src="js/jquery.timepicker.min.js"></script>
   <script src="js/scrollax.min.js"></script>
-  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
+  <script src="https://maps.googleapis.com/maps/api/js?key=!!&sensor=false"></script>
   <script src="js/google-map.js"></script>
   <script src="js/main.js"></script>
     
